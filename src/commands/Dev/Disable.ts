908ae2e5f9a62805f506250b3468a457ae252104
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 12:40:53 +0300

    Update Disable.ts

--- src/commands/Dev/Disable.ts
+++ src/commands/Dev/Disable.ts
@@ -18,17 +18,17 @@ export default class Command extends BaseCommand {
         if (!this.client.config.mods?.includes(M.sender.jid)) return void null
         const split = joined.split('|')
         const key = split[0].toLowerCase().trim()
-        if (!key) return void (await M.reply(`Provide the command you want to disable`))
+        if (!key) return void (await M.reply(`اكتب الامر الي تبيه يتوقف`))
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
         if (!command) return void (await M.reply(`No command found`))
         if (await this.client.DB.disabledcommands.findOne({ command: command.config.command }))
-            return void M.reply(`${command.config.command} is already disabled`)
+            return void M.reply(`${command.config.command} اصلا متوقف`)
         await new this.client.DB.disabledcommands({
             command: command.config.command,
             reason: (split[1] || '').trim() || ''
         }).save()
         await M.reply(
-            `*${this.client.util.capitalize(command.config.command)}* is now Disabled${
+            `*${this.client.util.capitalize(command.config.command)}* توقف${
                 split[1] ? ` for ${split[1]}` : ''
             }`
         )
