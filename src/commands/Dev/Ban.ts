4b01fde2d91d2cfae3327df99e4d344c1535afc2
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 12:38:30 +0300

    Update Ban.ts

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -7,17 +7,17 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'ban',
-            description: 'Bans the tagged users',
+            description: 'يمنع شخص من استعمال البوت',
             category: 'dev',
             usage: `${client.config.prefix}ban [@tag]`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ Only available to MODS')
+        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ الامر مسموح فقط للاونرز')
         const immortals = [M.sender, this.client.user.jid]
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
+        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('منشن الي تبيه ينمنع')
         let text = '*STATE*\n\n'
         for (const user of M.mentioned) {
             if (immortals.includes(user)) continue
@@ -25,11 +25,11 @@ export default class Command extends BaseCommand {
             const info = this.client.getContact(user)
             const username = info.notify || info.vname || info.name || user.split('@')[0]
             if (data?.ban) {
-                text += `🟨 ${username}: Already Banned\n`
+                text += `🟨 ${username}: اصلا مبند\n`
                 continue
             }
             await this.client.banUser(user)
-            text += `🟥 ${username}: Banned\n`
+            text += `🟥 ${username}: تم التبنيد\n`
         }
         M.reply(text)
     }
