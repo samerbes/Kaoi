290bbacde0cd47c96241982ba1879582591730d7
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 13:17:32 +0300

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
             adminOnly: true,
             aliases: ['boom', 'kick'],
             command: 'remove',
-            description: 'removes the mentioned users',
+            description: 'يطرد',
             category: 'moderation',
             usage: `${client.config.prefix}remove [@mention | tag]`,
             baseXp: 10
@@ -18,13 +18,13 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ مقدر ${this.config.command} لان ما معي اشراف`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
+        if (!M.mentioned.length) return void M.reply(`منشن الي تبيه ينطرد ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (M.groupMetadata?.admins?.includes(user)) M.reply(`❌ Skipped *${username}* as they're an admin`)
+            if (M.groupMetadata?.admins?.includes(user)) M.reply(`❌ سكب *${username}* لانه مشرف`)
             else {
                 await this.client.groupRemove(M.from, [user])
                 M.reply(`🏌️‍♂️Successfully Removed *${username}*`)
