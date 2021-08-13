efcef67ff6f953d562fe104c53dd7bd4f58d31c8
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 13:00:05 +0300

    Update Lyrics.ts

--- src/commands/Media/Lyrics.ts
+++ src/commands/Media/Lyrics.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'lyrics',
-            description: 'Gives you lyrics with song playable on WhatsApp',
+            description: 'يعطيك كلمات الاغنية',
             category: 'media',
             aliases: ['ly'],
             usage: `${client.config.prefix}yts [term]`,
@@ -23,15 +23,15 @@ export default class Command extends BaseCommand {
         })
     }
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('🔎 Provide a search term')
+        if (!joined) return void M.reply('🔎 اكتب اسم الاغنية')
         const term = joined.trim()       
         // get song from yts
         const { videos } = await yts(term + ' lyrics song')
-        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term *${term}*`)
+        if (!videos || videos.length <= 0) return void M.reply(`⚓ مافي فديو/اغنية كذا *${term}*`)
 
         const video = videos[0]
         const song = await getSong(term)
-        if (song.error || !song.data) return void M.reply(`❌ Could Not find any Matching songs: *${term}*`)
+        if (song.error || !song.data) return void M.reply(`❌ مافي فديو/اغنية كذا: *${term}*`)
         const { error, data } = await getLyrics(song.data)
         if (error || !data) return void M.reply(`❌ Could Not find any Matching Lyrics: *${song.data.title}*`)
         this.client.sendMessage(M.from, `*Lyrics of: ${term}*\n\n ${data}`, MessageType.text, {
