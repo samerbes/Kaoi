1a04b9116090f40995fb32394e0356cdca05ebd2
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 12:48:52 +0300

    Update Unban.ts

--- src/commands/Dev/Unban.ts
+++ src/commands/Dev/Unban.ts
@@ -7,28 +7,28 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'unban',
-            description: 'Unbans the tagged users',
+            description: 'يفك الباند عن احد للمستخدمين',
             category: 'dev',
             usage: `${client.config.prefix}unban [@tag]`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ Only available to MODS')
+        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ الامر مسموح فقط للاونرز')
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length || !M.mentioned[0])
-            return void M.reply('Please mention the user whom you want to unban')
+            return void M.reply('منشن الشخص الي ابي ينفك عنه الباند')
         let text = '*STATE*\n\n'
         for (const user of M.mentioned) {
             const data = await this.client.getUser(user)
             const info = this.client.getContact(user)
             const username = info.notify || info.vname || info.name || user.split('@')[0]
             if (!data?.ban) {
-                text += `🟨 ${username}: Not Banned\n`
+                text += `🟨 ${username}: اصلا مو مبند\n`
                 continue
             }
             await this.client.unbanUser(user)
-            text += `🟩 ${username}: Unbanned\n`
+            text += `🟩 ${username}: تم فك الحظر\n`
         }
         M.reply(text)
     }
