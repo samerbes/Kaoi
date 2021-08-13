427c53375278c63bcfca42af9e2f9a00819a1879
samerbes <83581912+samerbes@users.noreply.github.com>
2021-08-13 13:05:02 +0300

    Update play.ts

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'play',
-            description: '🎵 play a song with just search term!',
+            description: '🎵 يحمل اغنية!',
             category: 'media',
             aliases: ['music'],
             usage: `${client.config.prefix}play [term]`,
@@ -19,13 +19,13 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('🔎 Provide a search term')
+        if (!joined) return void M.reply('🔎 اكتب اسم الاغنية')
         const term = joined.trim()
         const { videos } = await yts(term)
-        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
+        if (!videos || videos.length <= 0) return void M.reply(`⚓ مافي اغنية كذا : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('👾 Sending...')
+        M.reply('👾 ارسال....')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
